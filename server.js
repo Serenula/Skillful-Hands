ae8b32b8cceec235fb244e2c96c99ae98dd1a23a
Serenula <serenula@gmail.com>
2024-07-01 13:24:53 +0800

    minor update
    missing / at app.use

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/services", serviceRoutes);
 app.use("/api/vendors", vendorRoutes);
-app.use("api/reviews", reviewRoutes);
+app.use("/api/reviews", reviewRoutes);
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
