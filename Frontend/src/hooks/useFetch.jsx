70ad253359311f80d8386fb9c6140efdccf7e8fb
Serenula <serenula@gmail.com>
2024-07-04 21:37:38 +0800

    hook and Tanstack
    Created hooks and installed Tanstack

---
+++ Frontend/src/hooks/useFetch.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url, options) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url, options);
+        const jsonData = await response.json();
+
+        setData(jsonData);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url, options]);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
