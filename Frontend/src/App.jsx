07b3ff189307a96992d0f4da481679fdb6d1ee17
Serenula <serenula@gmail.com>
2024-07-05 09:39:59 +0800

    updated app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import ServicePage from "./components/servicePage";
 
 const queryClient = new QueryClient();
 
 function App() {
-  return <QueryClientProvider client={queryClient}></QueryClientProvider>;
+  const [accessToken, setAccessToken] = useState("");
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ServicePage value={{ accessToken, setAccessToken }}></ServicePage>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
