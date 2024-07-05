c0058849a24fdf9c8a161004d75266719eec3141
Serenula <serenula@gmail.com>
2024-07-05 20:50:24 +0800

    merged vendor and user into one auth

--- backend/src/model/Auth.js
+++ backend/src/model/Auth.js
@@ -2,14 +2,50 @@ const mongoose = require("mongoose");
 
 const AuthSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true },
-    email: { type: String, required: true },
-    hash: { type: String, required: true },
-    role: { type: String, enum: ["user", "vendor"], default: "user" },
-    address: { type: String, required: true },
-    created_at: { type: Date, default: Date.now },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    hash: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "vendor"],
+      default: "user",
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      enum: [
+        "Cleaning",
+        "Aircon Servicing",
+        "Plumbing",
+        "Pet Grooming",
+        "Personal Training",
+      ],
+      required: function () {
+        return this.role === "vendor";
+      },
+    },
+    created_at: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  { collection: "auth" }
+  {
+    collection: "auth",
+  }
 );
 
 module.exports = mongoose.model("Auth", AuthSchema);
