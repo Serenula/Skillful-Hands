7644ae27196af7602b77d2e9988ad64bc4831292
Serenula <serenula@gmail.com>
2024-07-05 12:04:02 +0800

    service page completed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ServicePage value={{ accessToken, setAccessToken }}></ServicePage>
+      <ServicePage />
     </QueryClientProvider>
   );
 }
--- Frontend/src/components/servicePage.jsx
+++ Frontend/src/components/servicePage.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import useFetch from "../hooks/useFetch";
+import { useQuery } from "@tanstack/react-query";
 
-const ServicePage = ({ token }) => {
-  const { data: services, error, isLoading } = useFetch(token);
+const FetchServices = () => {
+  const fetchData = useFetch();
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredSvc, setFilteredSvc] = useState([]);
+
+  const {
+    data: services,
+    error,
+    isLoading,
+  } = useQuery({
+    queryKey: ["services"],
+    queryFn: async () => {
+      const response = await fetchData("/api/services");
+      return response;
+    },
+  });
+
+  const handleSearch = () => {
+    if (services) {
+      const filtered = services.filtered((service) =>
+        service.title.toLowerCase().includes(searchTerm.toLowerCase)
+      );
+      setFilteredSvc(filtered);
+    }
+  };
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
@@ -10,13 +34,26 @@ const ServicePage = ({ token }) => {
   return (
     <div>
       <h1>Services</h1>
+      <div>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search for services"
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
       <ul>
-        {services.Map((service) => {
-          <li key={service._id}>{service.title}</li>;
-        })}
+        {services.map((service) => (
+          <li key={service._id}>
+            <h3>{service.title}</h3>
+            <p>{service.description}</p>
+            <p>Price:${service.price}</p>
+          </li>
+        ))}
       </ul>
     </div>
   );
 };
 
-export default ServicePage;
+export default FetchServices;
