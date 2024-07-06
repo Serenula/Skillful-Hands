e2d325662380e1b72d4dec976c155803ef7f1240
xxiuminn <xiumin1997@gmail.com>
2024-07-06 17:03:37 +0800

    Update auth.js

--- backend/src/routers/auth.js
+++ backend/src/routers/auth.js
@@ -1,23 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const {
-  getAllUsers,
-  register,
-  login,
-  refresh,
-} = require("../controllers/auth");
+const { register, login, refresh, getAll } = require("../controllers/auth");
 const {
   validateRegistration,
   validateLogin,
   validateRefresh,
 } = require("../validators/auth");
 const checkErrors = require("../validators/checkErrors");
-const { seedVendors } = require("../controllers/vendor");
+// const { seedVendors } = require("../controllers/vendor");
 
-router.get("/users", getAllUsers);
+router.get("/all", getAll);
 router.put("/register", validateRegistration, checkErrors, register);
 router.post("/login", validateLogin, checkErrors, login);
 router.post("/refresh", validateRefresh, checkErrors, refresh);
-router.post("/seed", seedVendors);
+// router.post("/seed", seedVendors);
 
 module.exports = router;
